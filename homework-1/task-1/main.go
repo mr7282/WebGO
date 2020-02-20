d45a7fdd7af1327609191cd975ee844644c5d090
mr7282 <mr7282@mail.ru>
2020-02-20 22:33:17 +0300

    На пути к цели. Пробуем...

--- homework-1/task-1/main.go
+++ homework-1/task-1/main.go
@@ -5,9 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world!")
+	a := GetRequest()
+	b := GetReferenceSite()
+	fmt.Println(a)
+	fmt.Println(b)
 }
 
-func GetRequest() {
-	
+//GetRequest - получает строку поискового запроса
+func GetRequest() string {
+	var StrRequest string
+	fmt.Println("Введите поисковый запрос:")
+	fmt.Scan(&StrRequest)
+	return StrRequest
+}
+
+//GetReferenceSite - получает сайты на которых необходимо искать
+func GetReferenceSite() []string {
+	var ArrReference []string
+	var GetSingleRef string
+	fmt.Println("введите названия сайтов на которых поискать")
+	fmt.Scanln(&GetSingleRef)
+	ArrReference = append(ArrReference, GetSingleRef)
+	// 		for i := 0; i > 3; i++ {}
+	return ArrReference
+
 }
