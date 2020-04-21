d5066d3395dd82c5371431e3db6e8a960011226f
mr7282 <mr7282@mail.ru>
2020-04-22 01:09:46 +0300

    Старт выполнения второго задания

---
+++ homework-2/task-2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	
+)
+
+// Напишите два роута: один будет записывать информацию в Cookie (например, имя), а второй — получать ее и выводить в ответе на запрос.
+
+https://docs.google.com/document/d/1NdWh7wMM0hrgZbarOwaQsAskCN7AiygE4pRl9WzyD_k/edit#
+
+func main() {
+
+}
